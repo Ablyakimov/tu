c74b9d3a402c91328a602740b3b58e6b2dc3956c
Ablyakimov <unted575@gmail.com>
2021-04-14 14:33:19 +0300

    add hero

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    const video = $('.hero-video__video')[0]
+
+    // Кнопка play
+    $('.hero-video__play').click(function (e) { 
+        if($(this).hasClass('hero-video__play--active')){
+            $(this).removeClass('hero-video__play--active');
+            video.pause()
+        } else {            
+            $(this).addClass('hero-video__play--active');
+            video.play()
+        }
+    });
+
+    // Кнопка звука
+    $('.hero-video__volume').click(function () { 
+        if($(this).hasClass('hero-video__volume--mute')){
+            $(this).removeClass('hero-video__volume--mute');
+            $(this).addClass('hero-video__volume--unmute');
+            video.muted = !video.muted
+        } else {            
+            $(this).removeClass('hero-video__volume--unmute');
+            $(this).addClass('hero-video__volume--mute');            
+            video.muted = !video.muted
+        }
+     })
+});
